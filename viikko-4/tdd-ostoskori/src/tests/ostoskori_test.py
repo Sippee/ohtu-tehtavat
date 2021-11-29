3044157dda9c951d945f760678d9e32faf0a65d5
Sippee <sippegaming@gmail.com>
2021-11-30 01:03:43 +0200

    Testattu lisätään 2 samaa tuotetta ja toinen poistetaan

--- viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -92,3 +92,13 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(ostos.tuote, "Maito")
         self.assertEqual(ostos.lukumaara(), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_poistetaan_toinen(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+
+        self.assertEqual(ostos.lukumaara(), 1)
